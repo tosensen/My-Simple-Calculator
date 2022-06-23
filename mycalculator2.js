85a6ce2e72c047a8a9f750da6154759e4a04e04f
Adedapo Oluwatosin <janetoluwatosin2015@gmail.com>
2022-06-23 21:50:33 +0100

    changed to let

--- mycalculator2.js
+++ mycalculator2.js
@@ -1,49 +1,23 @@
-function add(a, b) {
-    return a + b;
-}
-function subtract(a, b) {
-    return a - b;
-}
-function multiply(a, b) {
-    return a * b;
-}
-function divide(a, b) {
-    return a / b;
-}
-function modulus(a, b) {
-    return a % b;
-}
+let first = parseInt(prompt("Enter your first number"));
+let operator = prompt("Enter operator you wish to use from the list below: +, -, *, /, %");
+let second = parseInt(prompt("Enter your second number"));
 
-
-
-
-
-
-function calculate() {
-    var a = parseInt(prompt("Enter the first number"));
-    var b = parseInt(prompt("Enter the second number"));
-    var operator = prompt("Enter operator you wish to use from the list below: +, -, *, /, %");
-    var result;
-    switch (operator) {
-        case "+":
-            result = add(a, b);
-            break;
-        case "-":
-            result = subtract(a, b);
-            break;
-        case "*":
-            result = multiply(a, b);
-            break;
-        case "/":
-            result = divide(a, b);
-            break;
-        case "%":
-            result = modulus(a, b);
-            break;
-        default:
-            alert("Invalid operator");
-            break;
-    }
-    alert(result);
+if (operator === "+") {
+    let addition = first + second;
+    alert(addition);
+} else if(operator ==="-"){
+    let subtraction = first - second;
+    alert(subtraction);
+} else if(operator ==="*"){
+    let multiplication = first * second;
+    alert(multiplication);
+} else if(operator ==="/"){
+    let division = first / second;
+    alert(division);
+} else if(operator ==="%"){
+    let modulus = first % second;
+    alert(modulus);
+} else (
+    alert("Invalid Inputs");
 }
-calculate();
+    
